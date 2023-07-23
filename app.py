f21a7c8e5fb1bb570bf0fb660b85a6d192393e61
Spoorthi2901 <spoorthi.s2901@gmail.com>
2023-07-23 22:49:04 +0530

    removed vercel configs

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def index():
     return render_template("index.html")
 
 
-# if __name__=="__main__":
-#     app.run(threaded=True,port=5000) 
+if __name__=="__main__":
+    app.run(threaded=True,port=5000) 
